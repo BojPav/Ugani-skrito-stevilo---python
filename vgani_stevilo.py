694485feaf730bbf2c720a5564ac5d6e1c9f933f
Bojan Pavlovic <pavlovicbojan86@gmail.com>
2016-09-19 21:59:36 +0200

    Vgani stevilo

---
+++ vgani_stevilo.py
@@ -0,0 +1,9 @@
+stevilka = "5"
+
+while True:
+    vneseno_stevilo = raw_input("Vgani skrito stevilo: ")
+    if vneseno_stevilo == "5":
+        print "cestitam"
+
+    else:
+        print "napacna stevilka"
