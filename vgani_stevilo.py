1821c8d18425f199ac634ba3460763f9c757e87b
Bojan Pavlovic <pavlovicbojan86@gmail.com>
2016-09-19 22:17:40 +0200

    Update vgani_stevilo.py

--- vgani_stevilo.py
+++ vgani_stevilo.py
@@ -4,6 +4,6 @@ while True:
     vneseno_stevilo = raw_input("Vgani skrito stevilo: ")
     if vneseno_stevilo == "5":
         print "cestitam"
-
+        break
     else:
         print "napacna stevilka"
